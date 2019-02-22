1d022b4acccba7e442f9d7cc02ea9da08fbc0b20
Doug Wright <mrdougwright@gmail.com>
2019-02-22 15:15:18 -0800

    1st pass

--- lib/sudoku.ex
+++ lib/sudoku.ex
@@ -1,2 +1,31 @@
 defmodule Sudoku do
+  def valid_row?(row) do
+    row
+    |> Enum.any?(&(&1 == '.'))
+    |> case do
+      true -> true
+      _ -> valid_row_of_nums?(row)
+    end
+  end
+
+  defp valid_row_of_nums?(row) do
+    row
+    |> Enum.uniq()
+    |> Enum.count()
+    |> case do
+      9 -> true
+      _ -> false
+    end
+  end
+
+  def valid_column?(rows) when length(rows) == 9 do
+    true
+  end
+
+  def valid_column?(rows) do
+    false
+  end
+
+  def valid_sub_grid?(rows) do
+  end
 end
